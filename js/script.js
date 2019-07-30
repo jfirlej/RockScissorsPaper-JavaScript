2294f05a7fedf2140948431e405cacd5732281b4
Jakub Firlej <jakub.firlej@outlook.com>
2019-07-30 20:26:56 +0200

    add button rock,paper,scissors

--- js/script.js
+++ js/script.js
@@ -3,54 +3,74 @@ var argMoveId, argPlayerMove, argComputerMove, computerMove, playerMove, randomN
 /**
  * Describe this function...
  */
-function getMoveName(argMoveId) {
-  console.log('wywołano funkcję getMoveName z argumentem: ' + argMoveId);
-  if (argMoveId == 1) {
-    return 'kamień';
-  } 
-  else if (argMoveId ==2)
-  {
-      return 'papier';
-  }
-  else if (argMoveId == 3)
-  {
-      return 'nożyce';
-  }
-  else {
-    printMessage('Nie znam ruchu o id ' + argMoveId + '. Zakładam, że chodziło o "kamień".');
-    return 'kamień';
-  }
-}
+
+
+
+var argButtonName, buttonRock, buttonPaper,buttonScissors;
 
 /**
  * Describe this function...
  */
-function displayResult(argPlayerMove, argComputerMove) {
-  console.log('wywołano funkcję displayResults z argumentami: ' + argPlayerMove + ', ' + argComputerMove);
-  if (argPlayerMove == 'papier' && argComputerMove == 'kamień') {
-    printMessage('wygrywasz!');
-  } 
-  else if(argPlayerMove == 'nożyce' && argComputerMove == 'papier') {
-    printMessage('wygrywasz');
+function buttonClicked(argButtonName) {
+  clearMessages();
+  console.log(argButtonName + ' został kliknięty');
+  ////////////////
+  function getMoveName(argMoveId) {
+    console.log('wywołano funkcję getMoveName z argumentem: ' + argMoveId);
+    if (argMoveId == 1) {
+      return 'kamień';
+    } 
+    else if (argMoveId ==2)
+    {
+        return 'papier';
+    }
+    else if (argMoveId == 3)
+    {
+        return 'nożyce';
+    }
+    else {
+      printMessage('Nie znam ruchu o id ' + argMoveId + '. Zakładam, że chodziło o "kamień".');
+      return 'kamień';
+    }
   }
-  else if (argPlayerMove == 'kamień' && argComputerMove == 'nożyce'){
-    printMessage('wygrywasz!');
+  
+  /**
+   * Describe this function...
+   */
+  function displayResult(argPlayerMove, argComputerMove) {
+    console.log('wywołano funkcję displayResults z argumentami: ' + argPlayerMove + ', ' + argComputerMove);
+    if (argPlayerMove == 'papier' && argComputerMove == 'kamień') {
+      printMessage('wygrywasz!');
+    } 
+    else if(argPlayerMove == 'nożyce' && argComputerMove == 'papier') {
+      printMessage('wygrywasz');
+    }
+    else if (argPlayerMove == 'kamień' && argComputerMove == 'nożyce'){
+      printMessage('wygrywasz!');
+    }
+    else if (argPlayerMove == argComputerMove)
+    {
+        printMessage('remis');
+    }
+    else {
+        printMessage('przegrywasz');
+    }
+    return printMessage('Zagrałem ' + argComputerMove + ', a Ty ' + argPlayerMove);
   }
-  else if (argPlayerMove == argComputerMove)
-  {
-      printMessage('remis');
-  }
-  else {
-      printMessage('przegrywasz');
-  }
-  return printMessage('Zagrałem ' + argComputerMove + ', a Ty ' + argPlayerMove);
+  playerMove = argButtonName;
+  
+  randomNumber = Math.floor(Math.random() * 3 + 1);
+  console.log('wylosowana liczba to: ' + randomNumber);
+  computerMove = getMoveName(randomNumber);
+  console.log('ruch komputera to: ' + computerMove);
+  displayResult(playerMove, computerMove);
+  //////////////
 }
-playerInput = prompt('Wybierz swój ruch! 1: kamień, 2: papier, 3: nożyce.');
-console.log('wybór ruchu gracza to: ' + playerInput);
-playerMove = getMoveName(playerInput);
-console.log('ruch gracza to: ' + playerMove);
-randomNumber = Math.floor(Math.random() * 3 + 1);
-console.log('wylosowana liczba to: ' + randomNumber);
-computerMove = getMoveName(randomNumber);
-console.log('ruch komputera to: ' + computerMove);
-displayResult(playerMove, computerMove);
+buttonRock = document.getElementById('button-rock');
+buttonRock.addEventListener('click', function(){ buttonClicked('kamień'); });
+
+buttonPaper=document.getElementById('button-paper');
+buttonPaper.addEventListener('click',function(){buttonClicked('papier');});
+
+buttonScissors = document.getElementById('button-scissors');
+buttonScissors.addEventListener('click',function(){buttonClicked('nozyce');});
